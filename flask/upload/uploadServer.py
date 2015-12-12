01bf52124133c9a9306f52dd087b1ee54055e6df
Shine <shine@Shines-MacBook-Pro.local>
2015-12-12 21:01:20 +0800

    After upload images , we can get resource url from server

--- flask/upload/uploadServer.py
+++ flask/upload/uploadServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,url_for,send_from_directory
 from werkzeug import secure_filename
 import os
 
@@ -18,12 +18,25 @@ def upload():
 	filename = secure_filename(imagefile.filename)
 	filePath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
 	if(os.path.isfile(filePath)):
-		return jsonify({'status':'file already exist'}),200
+		# return jsonify({'status':'file already exist'}),200
+		return redirect(url_for('uploaded_file',filename = filename))
 	else:
 		imagefile.save(filePath)
 		return jsonify({'status':'success'}), 200
 
+@app.route('/upload',methods = ['GET'])
+def getResources():
+	imageArray = []
+	for f in os.listdir(UPLOAD_FOLDER):
+		if f.endswith(".JPG"):
+			url = url_for('uploaded_file',filename = f,_external = True)
+			imageArray.append(url)
+	return jsonify({'status':'success','data':imageArray})
 
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 if __name__ == '__main__':
 	app.run(debug = True, host = '0.0.0.0')
